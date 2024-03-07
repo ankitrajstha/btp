782b58f632f2ba1681078175f44aa0a7e215c09e
Ankit B.K <ankitbk75@gmail.com>
2024-03-08 01:21:05 +0545

    feat: Persist draggable element order after page refresh
    This feature includes
    - Saving of order of draggable elements after the page request
    - User can rearranges the item according to the preferences
    - Ordering of elements are stored in browser's local storage

--- drag_n_drop/script.js
+++ drag_n_drop/script.js
@@ -4,7 +4,6 @@
  */
 import Sortable from '../node_modules/sortablejs/modular/sortable.complete.esm.js';
 
-
 /**
  * Parent elements whose children are made draggable
  */
@@ -12,17 +11,35 @@ const highlightsContainer = document.querySelector('.highlights-container');
 const upperChartsContainer = document.querySelector('.upper-charts-container');
 const lowerChartsContainer = document.querySelector('.lower-charts-container');
 
-
 /**
  * Common re-usable options for sortablejs
  */
 const commonOptions = {
   fallbackTolerance: 3, // So that we can select items on mobile
   animation: 150,
-  ghostClass: 'sortable-ghost',  // Class name for the drop placeholder
-}
+  ghostClass: 'sortable-ghost', // Class name for the drop placeholder
+  //store the current sorting order
+  store: {
+    get: (sortable) => {
+      var order = localStorage.getItem(sortable.options.group.name);
+      return order ? order.split("|") : [];
+    },
+    set: (sortable) => {
+      var order = sortable.toArray();
+      localStorage.setItem(sortable.options.group.name, order.join("|"));
+    },
+  },
+};
 
-const sortableHighlights = Sortable.create(highlightsContainer, commonOptions);
-const sortableUpperCharts = Sortable.create(upperChartsContainer, commonOptions);
-const sortableLowerCharts = Sortable.create(lowerChartsContainer, commonOptions);
-
+const sortableHighlights = Sortable.create(highlightsContainer, {
+  ...commonOptions,
+  group: "highlights-container", //group is used to store the same localKeyStorage across multiple sortable lists.
+});
+const sortableUpperCharts = Sortable.create(upperChartsContainer, {
+  ...commonOptions,
+  group: "upperchart-container",
+});
+const sortableLowerCharts = Sortable.create(lowerChartsContainer, {
+  ...commonOptions,
+  group: "lowerchart-container",
+});
