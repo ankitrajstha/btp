61661793a45aece1689ef5ceb0f3fef77af58a9f
prajanya <parajanya.shrestha@bajratechnologies.com>
2024-03-11 08:18:46 +0545

    feat: Adjust the layout of list items using flex to fill available space
    Also take maximum between heightRatio and widthRatio to change the font size

--- index.js
+++ index.js
@@ -1,24 +1,38 @@
 const resizableComponents = document.querySelectorAll('.resizable');
 
 resizableComponents.forEach(component => {
-  let fontSizeMultiplier = 1;
+  let prevWidth = component.offsetWidth;
+  let prevHeight = component.offsetHeight;
   component.addEventListener('mouseup', () => {
     const width = component.offsetWidth;
     const height = component.offsetHeight;
-    // console.log(height, width)
 
     const baseWidth = 449.33;
     const baseHeight = 129;
     const baseFontSizeH4 = 16;
+    const baseLineHeightH4 = 24;
     const baseFontSizeParagraph = 12;
+    const baseLineHeightParagrpah = 20;
+    if (width !== prevWidth || height !== prevHeight) {
+      const widthRatio = width / baseWidth;
+      const heightRatio = height / baseHeight;
 
-    const widthRatio = width / baseWidth;
-    const heightRatio = height / baseHeight;
+      const ratio = Math.max(widthRatio, heightRatio);
 
-    const newFontSizeH4 = baseFontSizeH4 * widthRatio;
-    const newFontSizeParagraph = baseFontSizeParagraph * widthRatio;
+      const newFontSizeH4 = baseFontSizeH4 * ratio;
+      const newLineHeightH4 = baseLineHeightH4 * ratio;
+      const newFontSizeParagraph = baseFontSizeParagraph * ratio;
+      const newLineHeightParagraph = baseLineHeightParagrpah * ratio;
 
-    component.style.setProperty('--base-font-size-h4', `${newFontSizeH4}px`);
-    component.style.setProperty('--base-font-size-paragraph', `${newFontSizeParagraph}px`);
+      component.style.setProperty('--base-font-size-h4', `${newFontSizeH4}px`);
+      component.style.setProperty('--base-line-height-h4', `${newLineHeightH4}px`);
+      component.style.setProperty('--base-font-size-paragraph', `${newFontSizeParagraph}px`);
+      component.style.setProperty('--base-line-height-paragraph', `${newLineHeightParagraph}px`);
+
+      prevWidth = width;
+      prevHeight = height;
+    }
+
+
   });
 });
