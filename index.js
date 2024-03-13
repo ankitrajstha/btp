490ac7b204e3ba7530df23d3e5e3e5e2d5a6cf40
prajanya <parajanya.shrestha@bajratechnologies.com>
2024-03-14 05:44:43 +0545

    feat: Changed the min and max width of each component and target font grow only in tech-debt paragraph

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import * as drag_n_drop from './drag_n_drop/script.js';
 const resizableComponents = document.querySelectorAll('.resizable');
-
+console.log("Hello")
 resizableComponents.forEach(component => {
   let prevWidth = component.offsetWidth;
   let prevHeight = component.offsetHeight;
-  component.addEventListener('mouseup', () => {
+  component.addEventListener('mousemove', () => {
     const width = component.offsetWidth;
     const height = component.offsetHeight;
 
@@ -22,13 +22,30 @@ resizableComponents.forEach(component => {
 
       const newFontSizeH4 = baseFontSizeH4 * ratio;
       const newLineHeightH4 = baseLineHeightH4 * ratio;
-      const newFontSizeParagraph = baseFontSizeParagraph * ratio;
-      const newLineHeightParagraph = baseLineHeightParagrpah * ratio;
+      // const newFontSizeParagraph = baseFontSizeParagraph * ratio;
+      // const newLineHeightParagraph = baseLineHeightParagrpah * ratio;
+      let newFontSizeParagraph, newLineHeightParagraph;
+      if (ratio > 1.3) {
+        newFontSizeParagraph = 12 * (ratio - 0.3);
+        newLineHeightParagraph = 20 * (ratio - 0.3);
+      }
+      if (newFontSizeParagraph && newLineHeightParagraph) {
+        // Target the paragraph with class technical-debt-content
+        const technicalDebtParagraph = component.querySelector('.technical-debt-content');
+        if (technicalDebtParagraph) {
+          const newFontSizeParagraph = baseFontSizeParagraph * ratio;
+          technicalDebtParagraph.style.fontSize = `${newFontSizeParagraph}px`;
+          technicalDebtParagraph.style.lineHeight = `${newLineHeightParagraph}px`;
+        }
+      } else {
+        component.style.removeProperty('--base-font-size-paragraph');
+        component.style.removeProperty('--base-line-height-paragraph');
+      }
 
       component.style.setProperty('--base-font-size-h4', `${newFontSizeH4}px`);
       component.style.setProperty('--base-line-height-h4', `${newLineHeightH4}px`);
-      component.style.setProperty('--base-font-size-paragraph', `${newFontSizeParagraph}px`);
-      component.style.setProperty('--base-line-height-paragraph', `${newLineHeightParagraph}px`);
+      // component.style.setProperty('--base-font-size-paragraph', `${newFontSizeParagraph}px`);
+      // component.style.setProperty('--base-line-height-paragraph', `${newLineHeightParagraph}px`);
 
       prevWidth = width;
       prevHeight = height;
