efc393480e15952f74ab094fcaaa39ca1f7b205f
Yubraj Maharjan <yubraj.maharjan@bajratechnologies.com>
2024-03-13 22:25:34 +0545

    refactor: move data for chart rendering to separate folder, 'apiData.js'

--- js/charts.js
+++ js/charts.js
@@ -1,3 +1,8 @@
+import { taskTypeData, taskTypelabels, taskStatusData, 
+  taskStatusLabels, storyPointsData, storyPointsLabels, 
+  logHoursData, logHoursLabels, clientDataData, clientDataLabels 
+} from "./static/apiData.js";
+
 const createRadialBarChart = (elementId, series) => {
   const chartElement = document.getElementById(elementId);
   function radialBarColor(series) {
@@ -247,88 +252,6 @@ document.addEventListener("DOMContentLoaded", () => {
   selectedChartTypeTaskType = selectedChartTypeTaskStatus = selectedChartTypeTaskTypeModal = selectedChartTypeTaskStatusModal = 'donut';
   selectedChartTypeStoryPoints = selectedChartTypeStoryPointsModal = selectedChartTypeLogHours = 'line';
   selectedChartTypeClientData = 'bar';
-  // Chart data
-  // Task type chart data 
-  const taskTypeData = [15, 6, 25, 30, 9, 15]; // Replace with actual data from API later
-  const taskTypelabels = [
-    "Bug Leakage",
-    "Task",
-    "Sub Task",
-    "Task Maintenance",
-    "Bug",
-    "Epic",
-  ]; // Replace with actual data from API later
-
-  // Task status chart data
-  const taskStatusData = [15, 6, 25, 30, 9, 15]; // Replace with actual data from API later
-  const taskStatusLabels = [
-    "Todo",
-    "In Progress",
-    "Awaiting Dev Review",
-    "In Review",
-    "Complete",
-    "Blocked",
-  ]; // Replace with actual data from API later
-
-  // Story points chart data
-  const storyPointsData = [
-    {
-      name: 'Completed',
-      type: 'column',
-      data: [35, 45, 32, 30, 22]
-    },
-    {
-      name: 'Assigned',
-      type: 'line',
-      data: [45, 50, 35, 40, 25]
-    }]; // Replace with actual data from API later
-  const storyPointsLabels = [
-    'Ram',
-    'Shyam',
-    'Hari',
-    'Shiva',
-    'Sita'
-  ]; // Replace with actual data from API later
-
-  // Log houts chart data
-  const logHoursData = [
-    {
-      name: 'Billable Hours',
-      type: 'line',
-      data: [60, 55, 60, 58, 52]
-    },
-    {
-      name: 'Available Hours',
-      type: 'column',
-      data: [38, 38, 28, 40, 21]
-    },
-    {
-      name: 'Estimated Hours',
-      type: 'column',
-      data: [38, 48, 36, 40, 21]
-    },
-    {
-      name: 'Logged Hours',
-      type: 'column',
-      data: [13, 22, 30, 19, 15]
-    }]; // Replace with actual data from API later
-  const logHoursLabels = [
-    'Ram',
-    'Shyam',
-    'Hari',
-    'Shiva',
-    'Sita'
-  ]; // Replace with actual data from API later
-
-
-  // Client Data chart data
-  const clientDataData = [3.1, 2.3, 10.1, 4]; // Replace with actual data from API later
-  const clientDataLabels = [
-    "CS Support",
-    "Dev Support",
-    "Client Call",
-    "Site Visit"
-  ]; // Replace with actual data from API later
 
   // Change graphs when another option is selected
   const chartOption = document.querySelector("#chart-type-1");
