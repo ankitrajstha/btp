6b5ada5f9ebb824d28965274c31b18e6da4e4ec4
Yubraj Maharjan <yubraj.maharjan@bajratechnologies.com>
2024-03-17 15:09:20 +0545

    refactor: change code to work with modular structure
    1. Comment code implementation of all other charts except task type chart
    2. Remove chartConfig.js and chartContainer.js from helper folder

--- js/charts.js
+++ js/charts.js
@@ -1,14 +1,9 @@
+import renderTaskTypeChart from "./charts/taskType/renderTaskTypeChart.js";
 import {
   taskTypeData, taskTypelabels, taskStatusData,
   taskStatusLabels, storyPointsData, storyPointsLabels,
   logHoursData, logHoursLabels, clientDataData, clientDataLabels
-} from "./static/apiData.js";
-import {
-  taskTypeContainer, taskStatusContainer, storyPointsContainer,
-  logHoursContainer, clientDataContainer
-} from "./static/chartContainers.js"
-import updateChart from "./helpers/render.js";
-import openModal from "./helpers/modal.js";
+} from "./static/apiData.js"
 
 const createRadialBarChart = (elementId, series) => {
   const chartElement = document.getElementById(elementId);
@@ -62,195 +57,197 @@ const createRadialBarChart = (elementId, series) => {
 };
 
 document.addEventListener("DOMContentLoaded", () => {
-  // Apex chart objects
-  let taskTypeChart, taskStatusChart, storyPointsChart, logHoursChart, clientDataChart;
-
-  // Selected chart type
-  let selectedChartTypeTaskType, selectedChartTypeTaskStatus, selectedChartTypeStoryPoints, selectedChartTypeLogHours, selectedChartTypeClientData;
-  let selectedChartTypeTaskStatusModal, selectedChartTypeTaskTypeModal, selectedChartTypeStoryPointsModal;
-
-  // Initialize chart type value
-  selectedChartTypeTaskType = selectedChartTypeTaskStatus = selectedChartTypeTaskTypeModal = selectedChartTypeTaskStatusModal = 'donut';
-  selectedChartTypeStoryPoints = selectedChartTypeStoryPointsModal = selectedChartTypeLogHours = 'line';
-  selectedChartTypeClientData = 'bar';
-
   // Intial chart render
-  taskTypeChart = updateChart(
-    taskTypeChart,
-    taskTypeContainer,
-    selectedChartTypeTaskType,
-    taskTypeData,
-    taskTypelabels,
-    false
-  );
-  taskStatusChart = updateChart(
-    taskStatusChart,
-    taskStatusContainer,
-    selectedChartTypeTaskStatus,
-    taskStatusData,
-    taskStatusLabels,
-    false
-  );
-  storyPointsChart = updateChart(
-    storyPointsChart,
-    storyPointsContainer,
-    selectedChartTypeStoryPoints,
-    storyPointsData,
-    storyPointsLabels,
-    false
-  );
-  logHoursChart = updateChart(
-    logHoursChart,
-    logHoursContainer,
-    selectedChartTypeLogHours,
-    logHoursData,
-    logHoursLabels,
-    false
-  );
-  clientDataChart = updateChart(
-    clientDataChart,
-    clientDataContainer,
-    selectedChartTypeClientData,
-    clientDataData,
-    clientDataLabels,
-    false
-  );
-  //radial chart render
+  renderTaskTypeChart(taskTypeData, taskTypelabels);
+  // //radial chart render
   createRadialBarChart("chart", 20);
   createRadialBarChart("chart2", 80);
+  // // Apex chart objects
+  // let taskTypeChart, taskStatusChart, storyPointsChart, logHoursChart, clientDataChart;
 
-  // Change graphs when another option is selected
-  // Ids for handling dropwdown event
-  const chartOption = document.querySelector("#chart-type-1");
-  const dropdownOptions = document.querySelector("#dropdownChartOptions");
-  const selectedChartLabel = document.querySelector("#selectedChart");
+  // // Selected chart type
+  // let selectedChartTypeTaskType, selectedChartTypeTaskStatus, selectedChartTypeStoryPoints, selectedChartTypeLogHours, selectedChartTypeClientData;
+  // let selectedChartTypeTaskStatusModal, selectedChartTypeTaskTypeModal, selectedChartTypeStoryPointsModal;
 
-  const chartOption2 = document.querySelector("#chart-type-2");
-  const dropdownOptions2 = document.querySelector("#dropdownChartOptions2");
-  const selectedChartLabel2 = document.querySelector("#selectedChart2");
+  // // Initialize chart type value
+  // selectedChartTypeTaskType = selectedChartTypeTaskStatus = selectedChartTypeTaskTypeModal = selectedChartTypeTaskStatusModal = 'donut';
+  // selectedChartTypeStoryPoints = selectedChartTypeStoryPointsModal = selectedChartTypeLogHours = 'line';
+  // selectedChartTypeClientData = 'bar';
 
-  chartOption.addEventListener("click", function () {
-    dropdownOptions.classList.toggle('dropdown-chart-options-visibility');
-  });
-  dropdownOptions.addEventListener("click", function (event) {
-    if (event.target.tagName === "LI") {
-      const selectedOptionText = event.target.textContent.trim();
-      selectedChartTypeTaskType = selectedChartTypeTaskTypeModal = event.target.getAttribute("data-value");
-      taskTypeChart = updateChart(
-        taskTypeChart,
-        taskTypeContainer,
-        selectedChartTypeTaskType,
-        taskTypeData,
-        taskTypelabels,
-        false
-      );
-      selectedChartLabel.textContent = selectedChartLabelModal.textContent = selectedOptionText;
-      dropdownOptions.style.display === "none";
-    }
-  });
+  // // Intial chart render
+  // taskTypeChart = updateChart(
+  //   taskTypeChart,
+  //   taskTypeContainer,
+  //   selectedChartTypeTaskType,
+  //   taskTypeData,
+  //   taskTypelabels,
+  //   false
+  // );
+  // taskStatusChart = updateChart(
+  //   taskStatusChart,
+  //   taskStatusContainer,
+  //   selectedChartTypeTaskStatus,
+  //   taskStatusData,
+  //   taskStatusLabels,
+  //   false
+  // );
+  // storyPointsChart = updateChart(
+  //   storyPointsChart,
+  //   storyPointsContainer,
+  //   selectedChartTypeStoryPoints,
+  //   storyPointsData,
+  //   storyPointsLabels,
+  //   false
+  // );
+  // logHoursChart = updateChart(
+  //   logHoursChart,
+  //   logHoursContainer,
+  //   selectedChartTypeLogHours,
+  //   logHoursData,
+  //   logHoursLabels,
+  //   false
+  // );
+  // clientDataChart = updateChart(
+  //   clientDataChart,
+  //   clientDataContainer,
+  //   selectedChartTypeClientData,
+  //   clientDataData,
+  //   clientDataLabels,
+  //   false
+  // );
 
-  chartOption2.addEventListener("click", function () {
-    dropdownOptions2.classList.toggle('dropdown-chart-options-visibility');
-  });
-  dropdownOptions2.addEventListener("click", function (event) {
-    if (event.target.tagName === "LI") {
-      const selectedOptionText = event.target.textContent.trim();
-      selectedChartTypeTaskStatus = selectedChartTypeTaskStatusModal = event.target.getAttribute("data-value");
-      taskStatusChart = updateChart(
-        taskStatusChart,
-        taskStatusContainer,
-        selectedChartTypeTaskStatus,
-        taskStatusData,
-        taskStatusLabels,
-        false
-      );
-      selectedChartLabel2.textContent = selectedChartLabelModal2.textContent = selectedOptionText;
-    }
-    dropdownOptions2.style.display === "none";
-  });
+  // // Change graphs when another option is selected
+  // // Ids for handling dropwdown event
+  // const chartOption = document.querySelector("#chart-type-1");
+  // const dropdownOptions = document.querySelector("#dropdownChartOptions");
+  // const selectedChartLabel = document.querySelector("#selectedChart");
 
-  // Charts inside modal
-  // Modal apex chart objects
-  let taskTypeModalChart, taskStatusModalChart, storyPointsModalChart, logHoursModalChart, clientDataModalChart;
+  // const chartOption2 = document.querySelector("#chart-type-2");
+  // const dropdownOptions2 = document.querySelector("#dropdownChartOptions2");
+  // const selectedChartLabel2 = document.querySelector("#selectedChart2");
 
-  // Ids for handling dropdown inside modal
-  const chartOptionModal = document.querySelector("#chart-type-1-0");
-  const dropdownOptionsModal = document.querySelector("#dropdownChartOptions-0");
-  const selectedChartLabelModal = document.querySelector("#selectedChart-0");
+  // chartOption.addEventListener("click", function () {
+  //   dropdownOptions.classList.toggle('dropdown-chart-options-visibility');
+  // });
+  // dropdownOptions.addEventListener("click", function (event) {
+  //   if (event.target.tagName === "LI") {
+  //     const selectedOptionText = event.target.textContent.trim();
+  //     selectedChartTypeTaskType = selectedChartTypeTaskTypeModal = event.target.getAttribute("data-value");
+  //     taskTypeChart = updateChart(
+  //       taskTypeChart,
+  //       taskTypeContainer,
+  //       selectedChartTypeTaskType,
+  //       taskTypeData,
+  //       taskTypelabels,
+  //       false
+  //     );
+  //     selectedChartLabel.textContent = selectedChartLabelModal.textContent = selectedOptionText;
+  //     dropdownOptions.style.display === "none";
+  //   }
+  // });
 
-  const chartOptionModal2 = document.querySelector("#chart-type-2-0");
-  const dropdownOptionsModal2 = document.querySelector("#dropdownChartOptions2-0");
-  const selectedChartLabelModal2 = document.querySelector("#selectedChart2-0");
+  // chartOption2.addEventListener("click", function () {
+  //   dropdownOptions2.classList.toggle('dropdown-chart-options-visibility');
+  // });
+  // dropdownOptions2.addEventListener("click", function (event) {
+  //   if (event.target.tagName === "LI") {
+  //     const selectedOptionText = event.target.textContent.trim();
+  //     selectedChartTypeTaskStatus = selectedChartTypeTaskStatusModal = event.target.getAttribute("data-value");
+  //     taskStatusChart = updateChart(
+  //       taskStatusChart,
+  //       taskStatusContainer,
+  //       selectedChartTypeTaskStatus,
+  //       taskStatusData,
+  //       taskStatusLabels,
+  //       false
+  //     );
+  //     selectedChartLabel2.textContent = selectedChartLabelModal2.textContent = selectedOptionText;
+  //   }
+  //   dropdownOptions2.style.display === "none";
+  // });
 
-  // Get modal open button and handle event
-  const openModalButton = document.getElementById("openTaskTypeModal");
-  openModalButton.addEventListener("click", () => {
-    openModal(
-      taskTypeModalChart,
-      "taskTypeModal",
-      "taskTypeModalChart",
-      selectedChartTypeTaskTypeModal,
-      taskTypeData,
-      taskTypelabels,
-      chartOptionModal,
-      dropdownOptionsModal,
-      selectedChartLabelModal
-    );
-  });
-  const openModalButton2 = document.getElementById("openTaskStatusModal");
-  openModalButton2.addEventListener("click", () => {
-    openModal(
-      taskStatusModalChart,
-      "taskStatusModal",
-      "taskStatusModalChart",
-      selectedChartTypeTaskStatusModal,
-      taskStatusData,
-      taskStatusLabels,
-      chartOptionModal2,
-      dropdownOptionsModal2,
-      selectedChartLabelModal2
-    );
-  });
-  const openModalButton3 = document.getElementById("openStoryPointsModal");
-  openModalButton3.addEventListener("click", () => {
-    openModal(
-      storyPointsModalChart,
-      "storyPointsModal",
-      "storyPointsModalChart",
-      selectedChartTypeStoryPointsModal,
-      storyPointsData,
-      storyPointsLabels,
-      null,
-      null,
-      null
-    );
-  });
-  const openModalButton4 = document.getElementById("openLogHoursModal");
-  openModalButton4.addEventListener("click", () => {
-    openModal(
-      logHoursModalChart,
-      "logHoursModal",
-      "logHoursModalChart",
-      selectedChartTypeLogHours,
-      logHoursData,
-      logHoursLabels,
-      null,
-      null,
-      null
-    );
-  });
-  const openModalButton5 = document.getElementById("openClientDataModal");
-  openModalButton5.addEventListener("click", () => {
-    openModal(
-      clientDataModalChart,
-      "clientDataModal",
-      "clientDataModalChart",
-      selectedChartTypeClientData,
-      clientDataData,
-      clientDataLabels,
-      null,
-      null,
-      null
-    );
-  });
+  // // Charts inside modal
+  // // Modal apex chart objects
+  // let taskTypeModalChart, taskStatusModalChart, storyPointsModalChart, logHoursModalChart, clientDataModalChart;
+
+  // // Ids for handling dropdown inside modal
+  // const chartOptionModal = document.querySelector("#chart-type-1-0");
+  // const dropdownOptionsModal = document.querySelector("#dropdownChartOptions-0");
+  // const selectedChartLabelModal = document.querySelector("#selectedChart-0");
+
+  // const chartOptionModal2 = document.querySelector("#chart-type-2-0");
+  // const dropdownOptionsModal2 = document.querySelector("#dropdownChartOptions2-0");
+  // const selectedChartLabelModal2 = document.querySelector("#selectedChart2-0");
+
+  // // Get modal open button and handle event
+  // const openModalButton = document.getElementById("openTaskTypeModal");
+  // openModalButton.addEventListener("click", () => {
+  //   openModal(
+  //     taskTypeModalChart,
+  //     "taskTypeModal",
+  //     "taskTypeModalChart",
+  //     selectedChartTypeTaskTypeModal,
+  //     taskTypeData,
+  //     taskTypelabels,
+  //     chartOptionModal,
+  //     dropdownOptionsModal,
+  //     selectedChartLabelModal
+  //   );
+  // });
+  // const openModalButton2 = document.getElementById("openTaskStatusModal");
+  // openModalButton2.addEventListener("click", () => {
+  //   openModal(
+  //     taskStatusModalChart,
+  //     "taskStatusModal",
+  //     "taskStatusModalChart",
+  //     selectedChartTypeTaskStatusModal,
+  //     taskStatusData,
+  //     taskStatusLabels,
+  //     chartOptionModal2,
+  //     dropdownOptionsModal2,
+  //     selectedChartLabelModal2
+  //   );
+  // });
+  // const openModalButton3 = document.getElementById("openStoryPointsModal");
+  // openModalButton3.addEventListener("click", () => {
+  //   openModal(
+  //     storyPointsModalChart,
+  //     "storyPointsModal",
+  //     "storyPointsModalChart",
+  //     selectedChartTypeStoryPointsModal,
+  //     storyPointsData,
+  //     storyPointsLabels,
+  //     null,
+  //     null,
+  //     null
+  //   );
+  // });
+  // const openModalButton4 = document.getElementById("openLogHoursModal");
+  // openModalButton4.addEventListener("click", () => {
+  //   openModal(
+  //     logHoursModalChart,
+  //     "logHoursModal",
+  //     "logHoursModalChart",
+  //     selectedChartTypeLogHours,
+  //     logHoursData,
+  //     logHoursLabels,
+  //     null,
+  //     null,
+  //     null
+  //   );
+  // });
+  // const openModalButton5 = document.getElementById("openClientDataModal");
+  // openModalButton5.addEventListener("click", () => {
+  //   openModal(
+  //     clientDataModalChart,
+  //     "clientDataModal",
+  //     "clientDataModalChart",
+  //     selectedChartTypeClientData,
+  //     clientDataData,
+  //     clientDataLabels,
+  //     null,
+  //     null,
+  //     null
+  //   );
+  // });
 });
