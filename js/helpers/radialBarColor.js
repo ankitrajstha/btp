8a30ec337f12a5147070b611a38a85edb8af0573
Ankit B.K <ankitbk75@gmail.com>
2024-03-22 11:01:04 +0545

    fix: color issues in radial graph
    When the radial graph is given certain percentage for the graph, the color did not show which have been fixed

--- js/helpers/radialBarColor.js
+++ js/helpers/radialBarColor.js
@@ -1,11 +1,11 @@
 function getRadialBarColor(series) {
     if (series > 30 && series < 60) {
         return "#E9E906";
-    } else if (series < 30 && series > 10) {
+    } else if (series <= 30 && series >= 10) {
         return "#CC3333";
-    } else if (series < 10 && series > 0) {
+    } else if (series < 10 && series >= 0) {
         return "#2F4858";
-    } else if (series > 60 && series < 100) {
+    } else if (series >= 60 && series <= 100) {
         return "#00ff00";
     }
 }
